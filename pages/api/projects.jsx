1f4fffaac62e1f617b16062266bc9c5b48404482
Furkan <64602929+fuwk4n@users.noreply.github.com>
2023-06-13 17:18:06 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,16 +2,16 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://cdn.discordapp.com/avatars/691729067636031538/a_ef3cdf1163f5bb2c62068a34dfb7c87c.gif?size=1024",
+      image: "https://cdn.discordapp.com/avatars/691729067636031538/3d7584c80dfe767a07724d1549d36378.webp?size=1024",
       name: "Fuwk4n",
-      description: "Nesef alırım arada vermeyi unuturum keşke komple unutsam ",
+      description: "Buraları düzenlicem ",
       link: "https://discord.gg/GqnKFH2ds7",
     },
     {
       id: 2,
-      image: "https://cdn.discordapp.com/avatars/691729067636031538/a_ef3cdf1163f5bb2c62068a34dfb7c87c.gif?size=1024",
+      image: "https://cdn.discordapp.com/avatars/691729067636031538/3d7584c80dfe767a07724d1549d36378.webp?size=1024",
       name: "Fuwk4n",
-      description: "Nesef alan sıradan bir Furkan",
+      description: "Buraları düzenlicem",
       link: "https://discord.gg/GqnKFH2ds7",
     }
   ];
